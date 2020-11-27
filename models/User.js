b30185eb08101af1076b12b8cd378096b0630d35
Emmanuella <emmavduru@gmail.com>
2020-11-27 02:59:05 +0100

    Add JWT to the authentication

--- models/User.js
+++ models/User.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const { isEmail } = require("validator")
 
 const userSchema = mongoose.Schema({
     first_name: String,
     last_name: String,
-    username: String,
-    email: String,
+    username: {
+        type: String,
+        required: [true, "Please enter a username"],
+        lowercase: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter an email address"],
+        lowercase: true,
+        unique: true,
+        validate: [isEmail, "Please enter a valid email"]
+    },
     location: String,
     bio: String,
-    password: String,
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Your password should have a minimum of 6 characters"]
+    },
 });
 
 // Hash the password
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,38 @@
+require("dotenv").config();
 const router = require("express")();
 const User = require("../models/User");
-const { isEmail } = require("validator")
+const jwt = require("jsonwebtoken");
+
+// Handle Errors
+const handleErrors = (err) => {
+    const errors = { username: "", email: "", password: "" };
+
+    if (err.code === 11000) {
+        const key = Object.values(err.keyPattern);
+        if(key.includes("username")) {
+            err.username = "This username is already registered";
+        }
+
+        if(key.includes("email")) {
+            err.email = "This email is already registered";
+        }
+    }
+
+    if (err.message.includes("User validation failed")) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+}
+
+// Generate JWT
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = id => {
+    const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: maxAge
+    });
+    return token;
+}
 
 // Home route
 router.get("/", (req, res) => {
@@ -15,24 +47,27 @@ router.get("/signup", (req, res) => {
 
 router.post("/signup", async(req, res) => {
     const { username, email, password } = req.body;
-    
-    if (isEmail(email)) {
-        try {
-            const user = User.create({ username, email, password });
-            console.log(user);
-            res.json({ user });
-        } catch (err) {
-            res.json({ err });
-        }
-    } else {
-        const emailError = "Please enter a valid email";
-        res.json({ emailError });
+
+    try {
+        const user = User.create({ username, email, password });
+        console.log(user);
+        const token = createToken(user._id);
+        res.cookie("project_auth", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000
+        });
+        res.json({ user });
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.json({ errors });
     }
 })
 
-
+// Login route
 router.get("/login", (req, res) => {
     res.render("login");
 })
+
+
 
 module.exports = router;
